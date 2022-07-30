c370660fb84a8644439d666714772d48268fb697
Daiveed <jajadavidjid@gmail.com>
2022-07-30 23:00:26 +0100

    FIrst Commit, Only navbar worked on. Still working on the dropdown menu with javascript

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// FIRST DROPDOWN
+let js_dropdown_holder = document.querySelector(".js_dropdown_holder");
+let js_dropdown = document.querySelector(".js_dropdown");
+
+js_dropdown_holder.addEventListener("click", function () {
+  js_dropdown.classList.toggle("js_dropdown_active");
+  return;
+});
+
+// SECOND DROPDOWN
+let js_dropdown_holder_Two = document.querySelector(".js_dropdown_holder_Two");
+let js_dropdown_Two = document.querySelector(".js_dropdown_Two");
+
+js_dropdown_holder_Two.addEventListener("click", function () {
+  js_dropdown_Two.classList.toggle("js_dropdown_active");
+  return;
+});
+
+// THIRD DROPDOWN
+let js_dropdown_holder_Three = document.querySelector(
+  ".js_dropdown_holder_Three"
+);
+let js_dropdown_Three = document.querySelector(".js_dropdown_Three");
+
+js_dropdown_holder_Three.addEventListener("click", function () {
+  js_dropdown_Three.classList.toggle("js_dropdown_active");
+  return;
+});
